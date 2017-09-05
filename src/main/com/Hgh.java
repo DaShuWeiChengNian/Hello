51676542948b5e7572fc40fbe10926b0d4312e31
DaShuWeiChengNian <hgh910505@163.com>
2017-09-05 16:25:19 +0800

    需要合并

---
+++ src/main/com/Hgh.java
@@ -0,0 +1,13 @@
+/**
+ * Created by David on 2017/9/5.
+ */
+public class Hgh {
+    public static void main(String[] args){
+        System.out.println("分支。。。。");
+        heBin();
+    }
+
+    public static void heBin(){
+        System.out.println("合并分支！！！！");
+    }
+}
