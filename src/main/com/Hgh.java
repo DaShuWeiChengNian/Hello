212337b968b4a94759d422d9b96a034315617c6a
DaShuWeiChengNian <hgh910505@163.com>
2017-09-05 16:34:08 +0800

    ```

--- src/main/com/Hgh.java
+++ src/main/com/Hgh.java
@@ -5,11 +5,11 @@ public class Hgh {
     public static void main(String[] args){
         System.out.println("分支。。。。");
         heBin();
+        a();
     }
 
     public static void heBin(){
         System.out.println("合并分支！！！！");
-        a();
     }
 
     public static void a(){
