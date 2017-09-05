d3c08e1fc62167948552b8cb8c9cdd21f1ae7159
DaShuWeiChengNian <hgh910505@163.com>
2017-09-05 16:14:32 +0800

    heBin

--- src/main/com/Hello.java
+++ src/main/com/Hello.java
@@ -4,5 +4,9 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        heBin2();
+    }
+    public static void heBin2(){
+        System.out.println("！！！！");
     }
 }
