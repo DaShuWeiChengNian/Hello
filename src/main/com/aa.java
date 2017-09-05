d0a8c419c1ae6064c000fa7add7dd8e5ed4290e7
DaShuWeiChengNian <hgh910505@163.com>
2017-09-05 17:19:08 +0800

    aaa

---
+++ src/main/com/aa.java
@@ -0,0 +1,14 @@
+/**
+ * Created by David on 2017/9/5.
+ */
+public class aa {
+    private  String a;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
